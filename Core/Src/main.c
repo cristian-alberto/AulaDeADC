43229bd5dfd42a1a25f1fa871fec9026002233cb
cristian-alberto <cristian.alberto@uel.br>
2024-09-10 11:39:45 -0300

    Funcionando

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -89,8 +89,8 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ADC1_Init();
   MX_DMA_Init();
+  MX_ADC1_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
